171672e9dc9db1b15bdfaac0c0b4a54f5341b92f
TineshPandian <tineshpandian8@gmail.com>
2024-10-08 14:47:39 +0530

    Created toggle navbar

--- app/Navbar/page.jsx
+++ app/Navbar/page.jsx
@@ -1,18 +1,54 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const loggedInUser = sessionStorage.getItem("isLoggedIn");
+    setIsLoggedIn(loggedInUser === "true");
+  }, []);
+
+  const handleLoginClick = () => {
+    router.push("/SignIn");
+  };
+
+  const toggleDropdown = () => {
+    setDropdownOpen((prev) => !prev);
+  };
+
+  const openLogoutModal = () => {
+    setShowLogoutModal(true);
+    setDropdownOpen(false);
+  };
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+    setShowLogoutModal(false);
+    router.push("/SignIn");
+  };
+
+  const closeLogoutModal = () => {
+    setShowLogoutModal(false);
+  };
 
   return (
-    <nav className="bg-white px-8 py-4">
+    <nav className="bg-gray-950 px-8 py-4 text-white">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="text-black text-xl font-bold">
+        <div className="text-xl font-bold">
           <Link href="/User">BlogSite</Link>
         </div>
-        <div className="relative hidden md:flex ">
+        <div className="relative hidden md:flex">
           <input
             type="text"
             className="w-80 px-4 py-2 rounded-2xl text-black bg-slate-200"
@@ -22,33 +58,52 @@ function Navbar() {
           />
         </div>
 
-        <ul className="md:flex space-x-9 text-black hidden">
+        <ul className="md:flex space-x-9 text-white hidden">
           <li className="group">
-            <Link href="/User" className="relative">
-              Home
-              <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-700 group-hover:w-full"></span>
-            </Link>
+            <Link href="/User" className="relative transition duration-300 ease-in-out hover:text-gray-400">Home</Link>
           </li>
           <li className="group">
-            <Link href="/about" className="relative">
-              Add Blog
-              <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-700 group-hover:w-full"></span>
-            </Link>
+            <Link href="/about" className="relative transition duration-300 ease-in-out hover:text-gray-400">Add Blog</Link>
           </li>
           <li className="group">
-            <Link href="/contact" className="relative">
-              Contact
-              <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-700 group-hover:w-full"></span>
-            </Link>
+            <Link href="/contact" className="relative transition duration-300 ease-in-out hover:text-gray-400">Contact</Link>
           </li>
-          <li className="group">
-            <Link href="/SignIn" className="relative">
-              Login
-              <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-700 group-hover:w-full"></span>
-            </Link>
+          <li className="relative">
+            {isLoggedIn ? (
+              <div className="flex items-center">
+                <div onClick={toggleDropdown} className="cursor-pointer rounded-full bg-gray-600 h-8 w-8 flex items-center justify-center">
+                  <FontAwesomeIcon icon={faUser} className="text-white" />
+                </div>
+                {dropdownOpen && (
+                  <div className="absolute right-0 mt-48 w-48 bg-white text-black rounded-xl z-10">
+                    <div className="px-4 py-3 border-b border-gray-300 cursor-pointer">
+                      <Link href="/profile">Profile</Link>
+                    </div>
+                    <div className="px-4 py-3 border-b border-gray-200 cursor-pointer">
+                      <Link href="/about">Add Blog</Link>
+                    </div>
+                    <div onClick={openLogoutModal} className="px-4 py-3 cursor-pointer">Logout</div>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <Link href="/SignIn" onClick={handleLoginClick} className="hover:text-white transition duration-300 ease-in-out">Login</Link>
+            )}
           </li>
         </ul>
       </div>
+      {showLogoutModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <h2 className="text-lg font-semibold mb-4 text-black">Confirm Logout</h2>
+            <p className="mb-6 text-black">Are you sure you want to log out?</p>
+            <div className="flex justify-end space-x-4">
+              <button onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 rounded-xl hover:bg-red-600">OK</button>
+              <button onClick={closeLogoutModal} className="bg-gray-300 text-gray-800 px-4 py-2 rounded-xl hover:bg-gray-400">Cancel</button>
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
--- app/SignIn/page.jsx
+++ app/SignIn/page.jsx
@@ -1,50 +1,50 @@
 "use client";
+
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
-import loginService from '../../Services/Login.service'; 
+import loginService from '../../Services/Login.service';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMsg, setErrorMsg] = useState('');
   const [successMsg, setSuccessMsg] = useState('');
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     setErrorMsg('');
     setSuccessMsg('');
+    setLoading(true);
 
     try {
       const result = await loginService.loginUser(email, password);
       if (result && result.message === 'Login successful') {
+        sessionStorage.setItem('isLoggedIn', 'true');
         setSuccessMsg(result.message);
-        router.push('/User'); 
+        router.push('/User');
       } else {
         setErrorMsg(result.message);
       }
     } catch (error) {
-      console.error("Login error:", error); 
-      setErrorMsg('Please try again later'); 
+      console.error("Login error:", error);
+      setErrorMsg('Please try again later');
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div className="min-h-screen flex">
       <div className="w-1/2 flex justify-center items-center bg-purple-100">
-        <img
-          src="/images/login-logo.png" 
-          alt="Illustration"
-          className="h-96"
-        />
+        <img src="/images/login-logo.png" alt="Illustration" className="h-96" />
       </div>
       <div className="w-1/2 flex flex-col justify-center items-center bg-custom-blue">
         <div className="w-96 bg-white rounded-3xl p-8">
           <h2 className="text-3xl font-bold text-gray-800 text-center mb-6">Hello Again!</h2>
-          <p className="text-gray-600 text-center mb-6">
-            Welcome back, you've been missed!
-          </p>
+          <p className="text-gray-600 text-center mb-6">Welcome back, you've been missed!</p>
           <form onSubmit={handleLogin}>
             <div className="mb-4">
               <label className="block text-gray-700">Email</label>
@@ -54,7 +54,7 @@ function Login() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
-                className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500"
+                className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-none"
               />
             </div>
             <div className="mb-4">
@@ -65,26 +65,24 @@ function Login() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
-                className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500"
+                className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none "
               />
             </div>
             <div className="mb-6 mt-6">
               <button
                 type="submit"
-                className="w-full bg-red-500 hover:bg-red-600 text-white font-semibold py-2 rounded-lg transition duration-200"
+                disabled={loading}
+                className={`w-full ${loading ? 'bg-gray-400' : 'bg-red-500 hover:bg-red-600'} text-white font-bold py-2 rounded-lg transition duration-300`}
               >
-                Sign In
+                {loading ? 'Loading...' : 'Login'}
               </button>
             </div>
+            {errorMsg && <p className="text-red-600 text-center">{errorMsg}</p>}
+            {successMsg && <p className="text-green-600 text-center">{successMsg}</p>}
+            <p className="mt-4 text-center">
+              Don’t have an account? <Link href="/signup" className="text-red-600 ">Sign Up</Link>
+            </p>
           </form>
-          <p className="text-center text-gray-600">
-            Not a member?{' '}
-            <Link href="/signup" className="text-purple-600 hover:underline">
-              Register now
-            </Link>
-          </p>
-          {errorMsg && <p className="text-center text-red-600 mt-4">{errorMsg}</p>}
-          {successMsg && <p className="text-center text-green-600 mt-4">{successMsg}</p>}
         </div>
       </div>
     </div>
